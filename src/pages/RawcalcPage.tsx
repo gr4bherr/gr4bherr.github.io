f761a94dc94591951aa9ca318ba2251f36e975e0
gr4bherr <gr4bherr@fastmail.com>
2024-03-31 20:36:10 +0200

    github and email link

--- src/pages/RawcalcPage.tsx
+++ src/pages/RawcalcPage.tsx
@@ -4,19 +4,24 @@ const RawcalcPage = () => {
   return (
     <>
       <h1>raw calc</h1>
+      <p>
+        check the code out on&nbsp;
+        <a href="https://github.com/gr4bherr/rawcalc">github</a>
+      </p>
 
       <h3>privacy policy</h3>
       <p>
         raw calc does not use or collect any user data whatsoever, nor do any
         third-party partners, as there are none.
       </p>
-
       <h3>support</h3>
       <p>
-        if you encounter any bugs or have any questions, contact me via email:
-        gr4bherr+rawcalc@fastmail.com
+        if you encounter any bugs or have any questions, contact me via
+        email:&nbsp;
+        <a href="mailto: gr4bherr+rawcalc@fastmail.com">
+          gr4bherr+rawcalc@fastmail.com
+        </a>
       </p>
-
       <Outlet />
     </>
   );
